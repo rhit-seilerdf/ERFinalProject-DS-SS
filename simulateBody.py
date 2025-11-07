1476c9f932770f9318409fc53b0f483ceb741ace
Dan Seiler <johndoe@example.com>
2025-11-07 13:59:44 -0500

    fixed more

--- simulateBody.py
+++ simulateBody.py
@@ -11,7 +11,7 @@ p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 p.setGravity(0,0,-9.8)
 
 planeId = p.loadURDF("plane.urdf")
-robotId = p.loadURDF("robot.urdf")
+robotId = p.loadURDF("low-cost-arm.urdf")
 #p.loadSDF("boxes.sdf")
 
 pyrosim.Prepare_To_Simulate(robotId)
@@ -34,74 +34,74 @@ for i in range(duration):
     tp1[i] = np.sin(x * t[i]*2*np.pi) * np.pi/4  
     tp2[i] = np.cos(x * t[i]*2*np.pi) * np.pi/8
 
-    pyrosim.Set_Motor_For_Joint(bodyIndex= robotId, 
-                                jointName="0_1", 
-                                controlMode = p.POSITION_CONTROL,
-                                targetPosition = tp1[i + 2],
-                                maxForce = 500
-                                )
+    # pyrosim.Set_Motor_For_Joint(bodyIndex= robotId, 
+    #                             jointName="0_1", 
+    #                             controlMode = p.POSITION_CONTROL,
+    #                             targetPosition = tp1[i + 2],
+    #                             maxForce = 500
+    #                             )
     
-    pyrosim.Set_Motor_For_Joint(bodyIndex= robotId, 
-                                jointName="1_2", 
-                                controlMode = p.POSITION_CONTROL,
-                                targetPosition = tp2[i + 2],
-                                maxForce = 500
-                                ) 
+    # pyrosim.Set_Motor_For_Joint(bodyIndex= robotId, 
+    #                             jointName="1_2", 
+    #                             controlMode = p.POSITION_CONTROL,
+    #                             targetPosition = tp2[i + 2],
+    #                             maxForce = 500
+    #                             ) 
       
-    pyrosim.Set_Motor_For_Joint(bodyIndex= robotId, 
-                                jointName="0_3", 
-                                controlMode = p.POSITION_CONTROL,
-                                targetPosition = tp1[i],
-                                maxForce = 500
-                                )
+    # pyrosim.Set_Motor_For_Joint(bodyIndex= robotId, 
+    #                             jointName="0_3", 
+    #                             controlMode = p.POSITION_CONTROL,
+    #                             targetPosition = tp1[i],
+    #                             maxForce = 500
+    #                             )
     
-    pyrosim.Set_Motor_For_Joint(bodyIndex= robotId, 
-                                jointName="3_4", 
-                                controlMode = p.POSITION_CONTROL,
-                                targetPosition = tp2[i],
-                                maxForce = 500
-                                )    
+    # pyrosim.Set_Motor_For_Joint(bodyIndex= robotId, 
+    #                             jointName="3_4", 
+    #                             controlMode = p.POSITION_CONTROL,
+    #                             targetPosition = tp2[i],
+    #                             maxForce = 500
+    #                             )    
     
-    pyrosim.Set_Motor_For_Joint(bodyIndex= robotId, 
-                                jointName="0_5", 
-                                controlMode = p.POSITION_CONTROL,
-                                targetPosition = tp1[i],
-                                maxForce = 500
-                                )
+    # pyrosim.Set_Motor_For_Joint(bodyIndex= robotId, 
+    #                             jointName="0_5", 
+    #                             controlMode = p.POSITION_CONTROL,
+    #                             targetPosition = tp1[i],
+    #                             maxForce = 500
+    #                             )
     
-    pyrosim.Set_Motor_For_Joint(bodyIndex= robotId, 
-                                jointName="5_6", 
-                                controlMode = p.POSITION_CONTROL,
-                                targetPosition = tp2[i],
-                                maxForce = 500
-                                )    
+    # pyrosim.Set_Motor_For_Joint(bodyIndex= robotId, 
+    #                             jointName="5_6", 
+    #                             controlMode = p.POSITION_CONTROL,
+    #                             targetPosition = tp2[i],
+    #                             maxForce = 500
+    #                             )    
     
-    pyrosim.Set_Motor_For_Joint(bodyIndex= robotId, 
-                                jointName="0_7", 
-                                controlMode = p.POSITION_CONTROL,
-                                targetPosition = tp1[i],
-                                maxForce = 500
-                                )
+    # pyrosim.Set_Motor_For_Joint(bodyIndex= robotId, 
+    #                             jointName="0_7", 
+    #                             controlMode = p.POSITION_CONTROL,
+    #                             targetPosition = tp1[i],
+    #                             maxForce = 500
+    #                             )
     
-    pyrosim.Set_Motor_For_Joint(bodyIndex= robotId, 
-                                jointName="7_8", 
-                                controlMode = p.POSITION_CONTROL,
-                                targetPosition = tp2[i],
-                                maxForce = 500
-                                )    
+    # pyrosim.Set_Motor_For_Joint(bodyIndex= robotId, 
+    #                             jointName="7_8", 
+    #                             controlMode = p.POSITION_CONTROL,
+    #                             targetPosition = tp2[i],
+    #                             maxForce = 500
+    #                             )    
     
-    linkState = p.getLinkState(robotId,0)
-    posx[i] = linkState[0][0]
-    posy[i] = linkState[0][1]
-    posz[i] = linkState[0][2]
+    # linkState = p.getLinkState(robotId,0)
+    # posx[i] = linkState[0][0]
+    # posy[i] = linkState[0][1]
+    # posz[i] = linkState[0][2]
 
-    legsensor[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("2")
+    # legsensor[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("2")
 
-    tp1[i] = np.sin(x * t[i]*2*np.pi) * np.pi/4  
-    if legsensor[i] > 0:
-        tp2[i] = np.cos(x * t[i]*2*np.pi) * np.pi/4
-    else:
-        tp2[i] = 0
+    # tp1[i] = np.sin(x * t[i]*2*np.pi) * np.pi/4  
+    # if legsensor[i] > 0:
+    #     tp2[i] = np.cos(x * t[i]*2*np.pi) * np.pi/4
+    # else:
+    #     tp2[i] = 0
 
     time.sleep(1/60)
 
